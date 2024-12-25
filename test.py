d14fe232dfde753c449dd269670836545af02f22
fullstackkg <khalilhgayle@gmail.com>
2024-12-25 16:07:59 -0500

    Unit test for getting team roster by ID

--- test.py
+++
@@ -1,8 +0,0 @@
-from nba_api.stats.endpoints.commonteamroster import CommonTeamRoster
-from nba_api.stats.static.teams import _find_team_name_by_id
-
-team_roster: CommonTeamRoster = CommonTeamRoster(1610612739)
-print(team_roster.common_team_roster.get_dict()["data"])
-
-team = _find_team_name_by_id(1610612739)
-print(team)
--- test/test_teams.py
+++ test/test_teams.py
@@ -7,6 +7,7 @@ from fastapi import Response
 from fastapi.testclient import TestClient
 
 from main import app
+from players.player_summary import PlayerSummary
 
 
 @pytest.fixture
@@ -36,3 +37,41 @@ async def test_get_all_teams(client: TestClient, mock_get_all_team_data) -> None
         assert teams[2]["team_id"] is not None
         assert teams[3]["team_id"] is not None
         assert teams[4]["team_id"] is not None
+
+
+@pytest.mark.asyncio
+async def test_get_players_by_team(client: TestClient) -> None:
+    MOCK_PLAYER_DATA = [
+        {
+            "player_id": 2544,
+            "first_name": "LeBron",
+            "last_name": "James",
+            "birth_date": "DEC 30, 1984",
+            "height": "6-9",
+            "weight": "250",
+            "season_exp": 21,
+            "jersey": "23",
+            "position": "F",
+            "team_id": 1610612747,
+            "team_city": "Los Angeles",
+            "team_name": "Lakers",
+        }
+    ]
+
+    async def mock_get_all_team_players(team_id: int) -> List[PlayerSummary]:
+        return [PlayerSummary(**player) for player in MOCK_PLAYER_DATA]
+
+    with patch("main.get_all_team_players", new=mock_get_all_team_players):
+        response: Response = client.get(f"/teams/1610612747/players")
+
+        assert response.status_code == 200
+        players = response.json()
+
+        assert len(players) == 1
+        player = players[0]
+        assert player["player_id"] == 2544
+        assert player["first_name"] == "LeBron"
+        assert player["last_name"] == "James"
+        assert player["team_id"] == 1610612747
+        assert player["team_city"] == "Los Angeles"
+        assert player["team_name"] == "Lakers"
